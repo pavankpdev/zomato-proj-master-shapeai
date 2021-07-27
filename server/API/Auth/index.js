564b43691794973231be0c6ab69c2fb71faec0a5
pavanleton <pavankumarkpkp@gmail.com>
2021-07-27 19:48:56 +0530

    add signin route

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -26,4 +26,22 @@ Router.post("/signup", async (req, res) => {
   }
 });
 
+/*
+Route     /signin
+Des       Signin with email and password
+Params    none
+Access    Public
+Method    POST  
+*/
+Router.post("/signin", async (req, res) => {
+  try {
+    const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+
+    const token = user.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 export default Router;
--- server/database/user/index.js
+++ server/database/user/index.js
@@ -31,6 +31,19 @@ UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
   return false;
 };
 
+UserSchema.statics.findByEmailAndPassword = async ({ email, password }) => {
+  // check whether email exist
+  const user = await UserModel.findOne({ email });
+  if (!user) throw new Error("User does no exist!!!");
+
+  // Compare password
+  const doesPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!doesPasswordMatch) throw new Error("invalid Password!!!");
+
+  return user;
+};
+
 UserSchema.pre("save", function (next) {
   const user = this;
 

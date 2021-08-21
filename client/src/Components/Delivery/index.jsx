3cb992ea3349fa7a98e92eb0bf5db494086a1955
pavanleton <pavankumarkpkp@gmail.com>
2021-08-21 18:44:33 +0530

    add delivery section

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 import DeliveryCarousal from "./DeliveryCarousal";
@@ -6,11 +6,56 @@ import Brand from "./Brand";
 import RestaurantCard from "../RestaurantCard";
 
 const Delivery = () => {
+  const [restaurantList, setRestaurantList] = useState([
+    {
+      _id: "123456",
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/8/18443088/d26f90bcacb3caeb175479a90e1eddb2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      ],
+      name: "Samosa Party",
+      cuisine: ["Street Food", "Beverages", "Tea"],
+      averageCost: 100,
+      isPro: true,
+      isOff: 80,
+      durationOfdelivery: 47,
+      restaurantReviewValue: 4.1,
+    },
+    {
+      _id: "123456-3",
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/8/18443088/d26f90bcacb3caeb175479a90e1eddb2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      ],
+      name: "Samosa Party",
+      cuisine: ["Street Food", "Beverages", "Tea"],
+      averageCost: 100,
+      isPro: true,
+      isOff: 80,
+      durationOfdelivery: 47,
+      restaurantReviewValue: 4.1,
+    },
+    {
+      _id: "123456-2",
+      photos: [
+        "https://b.zmtcdn.com/data/pictures/chains/6/50036/db2271e47065db0a611237066279297b_featured_v2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      ],
+      name: "Al-Bek",
+      cuisine: ["Mughlai", "Arabian", "North Indian", "Chinese", "Seafood"],
+      averageCost: 100,
+      isPro: true,
+      isOff: 80,
+      durationOfdelivery: 47,
+      restaurantReviewValue: 4.1,
+    },
+  ]);
   return (
     <>
       <DeliveryCarousal />
       {/* <Brand /> */}
-      <RestaurantCard />
+      <div className="flex justify-between flex-wrap">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard {...restaurant} key={restaurant._id} />
+        ))}
+      </div>
     </>
   );
 };
--- client/src/Components/RestaurantCard.jsx
+++ client/src/Components/RestaurantCard.jsx
@@ -1,34 +1,44 @@
 import React from "react";
 import { AiTwotoneStar } from "react-icons/ai";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return (
     <>
-      <div className="bg-white p-2 w-full mb-20 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
+      <div className="bg-white p-4 mb-4  w-full rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
         <div className="w-full h-56 lg:h-64 relative">
           <div className="absolute w-full bottom-4 flex items-end justify-between">
             <div className="flex flex-col gap-2 items-start">
-              <span className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">Pro extra 10% off</span>
-              <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">₹80 OFF</span>
+              {props.isPro && (
+                <span className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">
+                  Pro extra 10% off
+                </span>
+              )}
+              {props.isOff && (
+                <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">
+                  ₹{`${props.isOff}`} OFF
+                </span>
+              )}
             </div>
-            <span className="bg-white bg-opacity-75 p-1 rounded	mr-3">42 min</span>
+            <span className="bg-white bg-opacity-75 p-1 rounded	mr-3">
+              {props.durationOfdelivery} min
+            </span>
           </div>
           <img
-            src="https://b.zmtcdn.com/data/pictures/chains/3/18343993/c0fba8fbbd5bdac58e0e5d3b277a5cd4_o2_featured_v2.jpg?output-format=webp"
+            src={props.photos.length && props.photos[0]}
             alt="food"
             className="w-full h-full rounded-2xl"
           />
         </div>
         <div className="my-2 flex flex-col gap-2">
           <div className="flex items-center justify-between">
-            <h4 className="text-xl font-medium">Behrouz Biryani</h4>
+            <h4 className="text-xl font-medium">{props.name}</h4>
             <span className="bg-green-800 text-white text-sm p-1 rounded flex items-center">
-              4.1 <AiTwotoneStar />
+              {props.restaurantReviewValue} <AiTwotoneStar />
             </span>
           </div>
           <div className="flex items-center justify-between text-gray-500">
-            <p>biryani, North Indian, Mughlai</p>
-            <p>₹ 350</p>
+            <p>{props.cuisine.join(", ")}</p>
+            <p>₹ {props.averageCost} for one</p>
           </div>
         </div>
       </div>

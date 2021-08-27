9e3dfe2aa6b8b6c4af5685320132c77789d64f57
pavanleton <pavankumarkpkp@gmail.com>
2021-08-27 17:02:05 +0530

    add map address

--- client/src/Page/Restaurant/Overview.jsx
+++ client/src/Page/Restaurant/Overview.jsx
@@ -73,6 +73,14 @@ const Overview = () => {
   const ratingChanged = (newRating) => {
     console.log(newRating);
   };
+
+  const getLatLong = (mapAddress) => {
+    return mapAddress?.split(",").map((item) => parseFloat(item));
+  };
+
+  console.log(
+    reduxState?.mapLocation?.split(",").map((item) => parseFloat(item))
+  );
   return (
     <>
       <div className="flex flex-col md:flex-row relative">
@@ -93,19 +101,15 @@ const Overview = () => {
           </div>
           <h4 className="text-lg font-medium my-4">Cuisines</h4>
           <div className="flex flex-wrap gap-2">
-            <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">
-              Street Food
-            </span>
-            <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">
-              Street Food
-            </span>
-            <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">
-              Street Food
-            </span>
+            {reduxState?.cuisine.map((data) => (
+              <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">
+                {data}
+              </span>
+            ))}
           </div>
           <div className="my-4">
             <h4 className="text-lg font-medium">Average Cost</h4>
-            <h6>₹100 for one order (approx.)</h6>
+            <h6>₹{reduxState?.averageCost} for one order (approx.)</h6>
             <small className="text-gray-500">
               Exclusive of applicable taxes and charges, if any
             </small>
@@ -150,10 +154,10 @@ const Overview = () => {
           </div>
           <div className="my-4 w-full  md:hidden flex flex-col gap-4">
             <Mapview
-              title="Mumbai Xpress"
-              phno="+911212121212"
-              mapLocation={[12.988134202889283, 77.59405893120281]}
-              address="15, Sigma Central Mall, Vasanth Nagar, Cunningham Road, Bangalore"
+              title={reduxState?.name}
+              phno={`+91${reduxState?.contactNumber}`}
+              mapLocation={getLatLong(reduxState?.mapLocation)}
+              address={reduxState?.address}
             />
           </div>
 
@@ -168,10 +172,10 @@ const Overview = () => {
           className="hidden md:flex md:w-4/12 sticky rounded-xl top-2 bg-white p-3 shadow-md flex flex-col gap-4"
         >
           <Mapview
-            title="Mumbai Xpress"
-            phno="+911212121212"
-            mapLocation={[12.988134202889283, 77.59405893120281]}
-            address="15, Sigma Central Mall, Vasanth Nagar, Cunningham Road, Bangalore"
+            title={reduxState?.name}
+            phno={`+91${reduxState?.contactNumber}`}
+            mapLocation={getLatLong(reduxState?.mapLocation)}
+            address={reduxState?.address}
           />
         </aside>
       </div>

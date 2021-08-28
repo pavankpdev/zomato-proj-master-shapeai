9fe3bd33e4c9fb1ea092672279f37f19d093d6ab
pavanleton <pavankumarkpkp@gmail.com>
2021-08-28 19:39:46 +0530

    add menu and photos

--- client/src/Page/Restaurant/Menu.jsx
+++ client/src/Page/Restaurant/Menu.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // component
 import MenuCollection from "../../Components/restaurant/MenuCollection";
+
+// redux actions
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
+
 const Menu = () => {
   const [menus, setMenus] = useState([]);
 
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.menuImage)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setMenus(images);
+      });
+    }
+  }, []);
   return (
     <>
       <div className="flex flex-wrap gap-3">
-        {menus.map((menu) => (
-          <MenuCollection {...menu} />
-        ))}
+        <MenuCollection menuTitle="Menu" pages={menus.length} image={menus} />
       </div>
     </>
   );
--- client/src/Page/Restaurant/Photos.jsx
+++ client/src/Page/Restaurant/Photos.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import ImageViewer from "react-simple-image-viewer";
 
 // components
 import PhotoCollection from "../../Components/restaurant/PhotosCollection";
+// redux actions
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
 
 const Photos = () => {
-  const [photos, setPhotos] = useState([
-    "https://b.zmtcdn.com/data/pictures/chains/4/50714/caf2322a5ccd61e11b2c8ae43ed56727.jpg",
-    "https://b.zmtcdn.com/data/pictures/chains/4/50714/ac920675f44becc0d846698796c421bd.jpg",
-    "https://b.zmtcdn.com/data/pictures/chains/4/50714/8f4c852f9f948f91feb9dfbdb42b3388.jpg",
-  ]);
+  const [photos, setPhotos] = useState([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [CurrentImg, setCurrentImg] = useState(0);
+
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.photos)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setPhotos(images);
+      });
+    }
+  }, []);
   const closeViewer = () => setIsMenuOpen(false);
 
   const openViewer = () => setIsMenuOpen(true);

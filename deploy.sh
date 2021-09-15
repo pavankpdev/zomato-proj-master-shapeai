d04e374505795c6955f01ac94db5c720ffa1cb21
Devakumar NM <89896602+devnm21-r@users.noreply.github.com>
2021-09-15 21:03:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh "ubuntu@ec2-65-1-144-62.ap-south-1.compute.amazonaws.com	" "cd app/zomato-proj-master-shapeai && git pull origin master && docker-compose up --build"
+ssh "ubuntu@ec2-65-1-144-62.ap-south-1.compute.amazonaws.com" "cd app/zomato-proj-master-shapeai && git pull origin master && docker-compose up --build"
